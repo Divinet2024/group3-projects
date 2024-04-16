e2b18ce898c95feb0f0a99be78f97d9994d0e637
tobechukwu eze <tochy2500@yahoo.com>
2024-04-16 03:22:27 +0100

    learning in progress

---
+++ Tobe-firstscrpt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+i must say that i learnt so much guys
+we should do this more offen
+it really helps to internalise the knowledge
+well done everyone
+see you in class next day
+sign out Tobe
+
